f1adf8eb33975043c6b84fdf9f5acaea948bf411
clapierre22 <clapierre@ithaca.edu>
2025-07-11 12:38:28 -0400

    Added attack_test.py for testing the various examples

--- src/attacks.py
+++ src/attacks.py
@@ -13,7 +13,7 @@ class attack:
         raise NotImplementedError("Subclasses should implement this method")
 
 class xpi_attack(attack):
-    # Cross Prompt Injection Attack
+    # Cross Prompt Injection Attack, detect the admin command and execute it
     def __init__(self, chatbot, id):
         super().__init__(chatbot, id)
 
@@ -23,7 +23,19 @@ class xpi_attack(attack):
     def valid_attack(self):
         return False
     
-class hallucination:
+class hallucination(attack):
+    # If prompt is not in training data, hallucinate a response
+    def __init__(self, chatbot, id):
+        super().__init__(chatbot, id)
+
+    def execute(self):
+        pass
+
+    def valid_attack(self):
+        return False
+    
+class data_leak(attack):
+    # When the user does not ask outright for data, but rather asks a questions that would require a data leak to answer
     def __init__(self, chatbot, id):
         super().__init__(chatbot, id)
 
